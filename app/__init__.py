a7e26c346f2906e7680538d72e01aa42acc59585
Bartosz Wiaderek <bartoszwiaderek@gmail.com>
2023-01-09 18:24:05 +0100

    Mqtt start method moved

--- app/__init__.py
+++ app/__init__.py
@@ -9,8 +9,7 @@ def on_connect(mqttc, obj, flags, rc):
 
 mqttc = mqtt.Client("Publisher")
 mqttc.on_connect = on_connect
-mqttc.connect("192.168.1.191", 2000)
-mqttc.loop_start()
+
 
 app = Flask(__name__)
 
--- run.py
+++ run.py
@@ -1,6 +1,9 @@
 from app import app
 from app import routes
+from app import mqttc
 
+mqttc.connect("192.168.1.191", 2000)
+mqttc.loop_start()
 
 if __name__ == "__main__":
     app.run(host='192.168.1.191', port=2500)
