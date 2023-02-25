e313c97a7c1d10a07b79156d438f41bb793d0310
Bartosz Wiaderek <bartoszwiaderek@gmail.com>
2023-02-25 22:48:10 +0100

    2v db

--- app/db_setup.py
+++ app/db_setup.py
@@ -14,10 +14,9 @@ class Devices(db.Model):
     _id = db.Column(db.Integer, primary_key=True)
     _type = db.Column(db.String, nullable=False)
     _mqttId = db.Column(db.String, unique=True, nullable=False)
-    _state = db.Column(db.Boolean, nullable=False)
     _status = db.Column(db.String, nullable=False)
     _settings = db.Column(
-        db.JSON(), nullable=False)
+        db.JSON(), nullable=True)
 
     def __repr__(self):
         return f"<Device: {self._mqttId}>"
@@ -27,7 +26,6 @@ class Rooms(db.Model):
     _id = db.Column(db.Integer, primary_key=True)
     _name = db.Column(db.String, nullable=False)
     _pearsonCount = db.Column(db.Integer, nullable=False)
-    _pearsonList = db.Column(db.Table, nullable=True)
     _temperature = db.Column(db.Float, nullable=False)
     _humidity = db.Column(db.Float, nullable=False)
 
