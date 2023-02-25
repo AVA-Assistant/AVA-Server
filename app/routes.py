05ec67758a9dfc7d8f02d2f4d1993cc59c155109
Bartosz Wiaderek <bartoszwiaderek@gmail.com>
2023-02-25 23:30:07 +0100

    New routes for new db

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from app import app, db, mqttc, socketio_app, rgb, onOff, rgbCct, brightness
+from app import app, db, mqttc, socketio_app, Devices
 from flask_socketio import emit
 import json
 
@@ -6,93 +6,28 @@ import json
 @socketio_app.on('setup')
 def setup(devices):
     for device in devices:
-        if (device["type"] == "onf"):
-            db_record = onOff.query.filter_by(
-                _mqttId=device["mqtt_Id"]).first()
-            if (db_record):
-                device["status"] = db_record._status
-                device["state"] = {'status': db_record._state}
-            else:
-                newDevice = onOff(
-                    _id=int(device["id"]), _mqttId=device["mqtt_Id"], _state=False, _status="Off")
-                db.session.add(newDevice)
-                db.session.commit()
-        elif (device["type"] == "brht"):
-            db_record = brightness.query.filter_by(
-                _mqttId=device["mqtt_Id"]).first()
-            if (db_record):
-                device["status"] = db_record._status
-                device["state"] = {
-                    "status": db_record._state, "value": db_record._value}
-            else:
-                newDevice = brightness(
-                    _id=int(device["id"]), _mqttId=device["mqtt_Id"], _state=False, _value=0, _status="Off")
-                db.session.add(newDevice)
-                db.session.commit()
-        elif (device["type"] == "rgb"):
-            db_record = rgb.query.filter_by(
-                _mqttId=device["mqtt_Id"]).first()
-            if (db_record):
-                device["status"] = db_record._status
-                device["state"] = {
-                    "status": db_record._state, "mode": db_record._mode, "value": db_record._value}
-            else:
-                newDevice = rgb(
-                    _id=int(device["id"]), _mqttId=device["mqtt_Id"], _state=False, _mode="auto", _value=0, _status="Off")
-                db.session.add(newDevice)
-                db.session.commit()
-        elif (device["type"] == "rgbcct"):
-            db_record = rgbCct.query.filter_by(
-                _mqttId=device["mqtt_Id"]).first()
-            if (db_record):
-                device["status"] = db_record._status
-                device["state"] = {
-                    "status": db_record._state, "mode": db_record._mode, "rgbValue": db_record._rgbValue, "cctValue": db_record._cctValue}
-            else:
-                newDevice = rgbCct(
-                    _id=int(device["id"]), _mqttId=device["mqtt_Id"], _state=False, _mode="auto", _rgbValue=0, _cctValue=0, _status="Off")
-                db.session.add(newDevice)
-                db.session.commit()
+        db_record = Devices.query.filter_by(
+            _mqttId=device["mqtt_Id"]).first()
+        if (db_record):
+            device["status"] = db_record._status
+            device["settings"] = db_record._settings
+        else:
+            newDevice = Devices(
+                _id=int(device["id"]), _type=device["type"], _mqttId=device["mqtt_Id"], _status="Off")
+            db.session.add(newDevice)
+            db.session.commit()
+
     emit("setup", devices)
 
 
 @socketio_app.on('changeState')
 def setup(device):
-    if (device["type"] == "onf"):
-        dbDev = onOff.query.filter_by(
-            _mqttId=device["mqtt_Id"]).first()
-        dbDev._state = device['state']['status']
-        dbDev._status = device['status']
-        db.session.commit()
-        mqttc.publish(device["mqtt_Id"], json.dumps(
-            {"state": device['state']['status']}))
-
-    elif (device["type"] == "brht"):
-        dbDev = brightness.query.filter_by(
-            _mqttId=device["mqtt_Id"]).first()
-        dbDev._state = device['state']['status']
-        dbDev._value = device['state']['value']
-        dbDev._status = device['status']
-        db.session.commit()
-        mqttc.publish(device["mqtt_Id"], json.dumps(
-            {"state": device['state']['status'], "value": device['state']['value']}))
-    elif (device["type"] == "rgb"):
-        dbDev = rgb.query.filter_by(
-            _mqttId=device["mqtt_Id"]).first()
-        dbDev._state = device['state']['status']
-        dbDev._value = device['state']['value']
-        dbDev._mode = device['state']['mode']
-        dbDev._status = device['status']
-        db.session.commit()
-    elif (device["type"] == "rgbcct"):
-        dbDev = rgbCct.query.filter_by(
-            _mqttId=device["mqtt_Id"]).first()
-        dbDev._state = device['state']['status']
-        dbDev._rgbValue = device['state']['rgb_Value']
-        dbDev._cctValue = device['state']['cct_Value']
-        dbDev._mode = device['state']['mode']
-        dbDev._status = device['status']
-        db.session.commit()
-
+    dbDev = Devices.query.filter_by(
+        _mqttId=device["mqtt_Id"]).first()
+    dbDev._settings = device['settings']
+    dbDev._status = device['status']
+    db.session.commit()
+    mqttc.publish(device["mqtt_Id"], json.dumps(
+        device['settings']))
     if device['emit']:
         emit("stateChanged", device, broadcast=True)
--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 from app import app, mqttc, routes, socketio_app
 
-mqttc.connect("192.168.1.191", 2000)
-mqttc.loop_start()
+# mqttc.connect("192.168.1.191", 2000)
+# mqttc.loop_start()
 
 if __name__ == "__main__":
-    socketio_app.run(app=app, host='192.168.1.191', port=2500)
+    socketio_app.run(app=app, host='192.168.1.190', port=2500)
